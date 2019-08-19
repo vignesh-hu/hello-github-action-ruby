f16196233ec2be19a255f3c01648a44228d8e1fd
YutaGoto <you.goto.510@gmail.com>
2019-08-20 00:05:09 +0900

    add sample

--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -8,11 +8,13 @@ describe Polygon do
     let(:triangle) { Polygon.new(3) }
     let(:rectangle) { Polygon.new(4) }
     let(:nonagon) { Polygon.new(9) }
+    let(:hexagon) { Polygon.new(6) }
 
     it do
       expect(triangle.interior_angles_sum).to eq 180
       expect(rectangle.interior_angles_sum).to eq 360
       expect(nonagon.interior_angles_sum).to eq 1260
+      expect(hexagon.interior_angles_sum).to eq 720
     end
   end
 
@@ -20,11 +22,13 @@ describe Polygon do
     let(:triangle) { Polygon.new(3) }
     let(:rectangle) { Polygon.new(4) }
     let(:nonagon) { Polygon.new(9) }
+    let(:hexagon) { Polygon.new(6) }
 
     it do
       expect(triangle.exterior_angles_sum).to eq 360
       expect(rectangle.exterior_angles_sum).to eq 360
       expect(nonagon.exterior_angles_sum).to eq 360
+      expect(hexagon.exterior_angles_sum).to eq 360
     end
   end
 end
